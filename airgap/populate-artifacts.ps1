26b1bb9807c9ca95d8f4e06708de56d8b9bc2022
iareanthony <144458779+iareanthony@users.noreply.github.com>
2026-07-20 17:25:32 -0400

    mirror Falco and Ollama runtime artifacts reproducibly

--- airgap/populate-artifacts.ps1
+++ airgap/populate-artifacts.ps1
@@ -9,13 +9,12 @@ Invoke-WebRequest $sources.cisaKev -OutFile "$artifactDir\known_exploited_vulner
 Invoke-WebRequest $sources.epssCsv -OutFile "$artifactDir\epss_scores-current.csv.gz"
 helm pull falco --repo https://falcosecurity.github.io/charts --version $sources.falcoCharts.falco --destination $chartDir
 helm pull falcosidekick --repo https://falcosecurity.github.io/charts --version $sources.falcoCharts.falcosidekick --destination $chartDir
-$extraImages = @($sources.trivyImage)
+$extraImages = @($sources.trivyImage) + @($sources.falcoImages) + @($sources.ollamaImage)
+$extraImages | ForEach-Object { docker pull $_ }
 if ($IncludeOllamaModel) {
   $ollamaDir = New-Item -ItemType Directory -Force "$artifactDir\ollama"
-  docker pull $sources.ollamaImage
   docker run --detach --rm --name xib-ollama-export --volume "$($ollamaDir.FullName):/root/.ollama" $sources.ollamaImage | Out-Null
   try { docker exec xib-ollama-export ollama pull $sources.ollamaModel } finally { docker stop xib-ollama-export | Out-Null }
-  $extraImages += $sources.ollamaImage
   Set-Content "$ollamaDir\MODEL" $sources.ollamaModel
 }
 docker save --output "$OutputDirectory\xib-artifact-images.tar" $extraImages
